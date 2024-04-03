2264c1db294da857a23e284355a9e05711609c63
Madhav Desai <md@Madhavs-MacBook-Air.local>
2024-04-03 11:58:06 +0530

    commited firs time with error

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://22it021:madhav21@cluster0.xw2iktt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  secretOrKey: "jfjkdjrhfybshrfg",
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// BodyParser Middleware
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// connect to mongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = 3002;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
